255645c37872be91e4292a42a638d5be4259fec2
Akshay <akshaymg99@gmail.com>
2021-03-14 14:01:55 +0530

    Added natural-log, power function and Illegal Argument checks

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,11 +5,11 @@ public class App
     public static void main(String args[])
     {
         int choice, flag=0;
-        double num1, num2;
+        double x=1, b=1;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Calculator");
+        System.out.println("Scientific Calculator");
         do {
-            System.out.println("1 for Addition\n2 for Subtraction\n3 for Multiplication\n4 for Division\n");
+            System.out.println("1 Square root\n2 Factorial\n3 Natural Log\n4 Power function\n");
             System.out.print("Enter your choice: ");
             choice = scan.nextInt();
             if (choice > 4 || choice <1) {
@@ -17,20 +17,25 @@ public class App
                 System.out.println("Invalid choice, exiting\n");
             }
             else if (flag != 1){
-            	 System.out.println("Enter two numbers");
-                 System.out.print("Enter number 1: ");
-                 num1 = scan.nextDouble();
-                 System.out.print("Enter number 2: ");
-                 num2 = scan.nextDouble();
-                 
+                if(choice != 4) {
+                    System.out.println("Enter input no x");
+                    x = scan.nextDouble();
+                }
+                else if(choice == 4) {
+                    System.out.println("Enter x (base no)");
+                    x = scan.nextDouble();
+                    System.out.println("Enter b (power no)");
+                    b = scan.nextDouble();
+                }
+
                  switch(choice) {
-                 case 1: System.out.println(num1+" + "+num2+" = "+Add(num1, num2));
+                 case 1: System.out.println( "Sqrt of "+x+" = "+Sqrt_fun(x));
                  		break;
-                 case 2: System.out.println(num1+" - "+num2+" = "+Subtract(num1, num2));
+                 case 2: System.out.println("Factorial of "+x+" = "+Factorial(x));
                  		break;
-                 case 3: System.out.println(num1+" * "+num2+" = "+Multiply(num1, num2));
+                 case 3: System.out.println("Natural log of"+x+" = "+Nat_log(x));
                  		break;
-                 case 4: System.out.println(num1+" / "+num2+" = "+Divide(num1, num2));
+                 case 4: System.out.println("Power of"+x+"^"+b+" = "+power_xb(x, b));
                 		break;
                 default: System.out.println("Exiting, Bye");
                 flag=1;
@@ -40,19 +45,37 @@ public class App
             }while(flag==0);
     }
     
-    static double Add(double a, double b) {
-    	return a + b;
+    static double Sqrt_fun(double x) {
+    	if (x <= 0) {
+    	    throw new IllegalArgumentException("Square root of zero and negative doesn't exits in Real number space");
+        }
+    	else
+    	    return Math.sqrt(x);
     }
-    static double Subtract(double a, double b) {
-    	return a - b;
+    static double Factorial(double x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Factorial doesn't exist for negative numbers");
+        }
+        else {
+            double fact = 1;
+            for(int i=2; i<=x; i++) {
+                fact = fact * i;
+            }
+            return fact;
+        }
     }
-    static double Multiply(double a, double b) {
-    	return a * b;
+    static double Nat_log(double x) {
+        if(x <= 0) {
+            throw new IllegalArgumentException("Number cannot be zero or negative");
+        }
+        else
+            return Math.log(x);
     }
-    static double Divide(double a, double b) {
-    	if (b == 0) {
-    		throw new IllegalArgumentException("Divisor cannot be zero, Exiting");
+    static double power_xb(double x, double b) {
+    	if (x == 0) {
+    		throw new IllegalArgumentException("Base cannot be zero");
     	}
-    	else return a / b;
+    	else
+    	    return Math.pow(x, b);
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -14,45 +14,36 @@ public class CalculatorTest {
 	    }
 
 		@Test
-	    public void testAdd() {
-	        double a = 15.0;
-	        double b = 20.0;
-	        double expectedResult = 35.0;
-	        double result = calculator.Add(a, b);
-	        Assert.assertEquals(expectedResult, result, 0);
-	    }
-		
-		@Test
-		public void testSubtract() {
-			double a = 34;
-	        double b = 12;
-	        double expectedResult = 22;
-	        double result = calculator.Subtract(a, b);
-	        Assert.assertEquals(expectedResult, result, 0);
-		}
-		
-		@Test
-		public void testMultiply() {
-			double a = 18.5;
-	        double b = 12.6;
-	        double expectedResult = 233.1;
-	        double result = calculator.Multiply(a, b);
-	        Assert.assertEquals(expectedResult, result, 0);
-		}
-		
-		@Test
-		public void testDivide() {
-			double a = 60.0;
-	        double b = 12.0;
+	    public void testSqrt_fun() {
+	        double x = 25;
 	        double expectedResult = 5.0;
-	        double result = calculator.Divide(a, b);
-	        Assert.assertEquals(expectedResult, result, 0.0001);
+	        double result = calculator.Sqrt_fun(x);
+	        Assert.assertEquals(expectedResult, result, 0);
+	    }
+		
+		@Test
+		public void testFactorial() {
+			double x = 5;
+	        double expectedResult = 120;
+	        double result = calculator.Factorial(x);
+	        Assert.assertEquals(expectedResult, result, 0);
 		}
 		
-		@Test(expected = IllegalArgumentException.class)
-	    public void testDivideByZero() {
-	        int a = 25;
-	        int b = 0;
-	        calculator.Divide(a, b);
-	    }
+		@Test
+		public void testNat_log() {
+			double x = 100;
+	        double expectedResult = 4.6;
+	        double result = calculator.Nat_log(x);
+	        Assert.assertEquals(expectedResult, result, 01);
+		}
+		
+		@Test
+		public void testpower_xb() {
+			double x = 9.0;
+	        double b = 2.0;
+	        double expectedResult = 81.0;
+	        double result = calculator.power_xb(x, b);
+	        Assert.assertEquals(expectedResult, result, 0.01);
+		}
+
 }
